83d5b547f6fc5d82bfc04ccc3b3bca1142227d51
ShrewdLuni <ander113q@gmail.com>
2024-04-29 22:58:09 +0300

    feat: add prisma db controller

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,11 +1,11 @@
 import express from "express";
 
-import { getUsers, getUserBySessionToken } from "../db/users";
 import { merge } from "lodash";
+import { db } from "./db";
 
 export const getAllUsers = async (req: express.Request, res: express.Response) => {
     try {
-        const users = await getUsers();
+        const users = await db.user.findMany();
 
         return res.status(200).json(users);
     } catch (error) {
@@ -35,11 +35,21 @@ export const getCurrentUser = async (req: express.Request, res: express.Response
     }
 } 
 
-// export const getUsers = () => UserModel.find();
-// export const getUserByEmail = (email: String) => UserModel.findOne({ email });
-// export const getUserBySessionToken = (sessionToken : String) => UserModel.findOne({'authentication.sessionToken' : sessionToken,});
-// export const getUserById = (id : String) => UserModel.findById(id);
+
+export const getUserBySessionToken = async (sessionToken: string) => {
+    try {
+        const users = await db.user.findMany();
+        return users
+    } catch (error) {
+        return error
+    }
+}
+
+// // export const getUsers = () => UserModel.find();
+// export const getUserByEmail = (email: String) => db.findOne({ email });
+// // export const getUserBySessionToken = (sessionToken : String) => UserModel.findOne({'authentication.sessionToken' : sessionToken,});
+// // export const getUserById = (id : String) => UserModel.findById(id);
 // export const createUser = (values: Record<string, any>) => new UserModel(values).save().then((user) => user.toObject());
-// export const deleteUserById = (id: String) => UserModel.findOneAndDelete({ _id: id});
-// export const updateUserById = (id: String, values: Record<string, any>) => UserModel.findByIdAndUpdate(id, values);
-// export const getUserByUsername = (username: String) => UserModel.findOne({ username }); 
+// // export const deleteUserById = (id: String) => UserModel.findOneAndDelete({ _id: id});
+// // export const updateUserById = (id: String, values: Record<string, any>) => UserModel.findByIdAndUpdate(id, values);
+// // export const getUserByUsername = (username: String) => UserModel.findOne({ username }); 
